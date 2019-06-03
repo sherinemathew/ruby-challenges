fe96e1bd1e2bd481d492654f8db2136bdf88c5e4
sherinemathew <sherine.mary.mathew@gmail.com>
2019-06-03 12:26:34 +1000

    challenge done

--- 11_fizzbuzz.rb
+++ 11_fizzbuzz.rb
@@ -26,4 +26,16 @@
 # Buzz
 # ...etc
 
-# Your code here
+#Defining a range from 1 to 100 and iterating through each number
+(1..100).each do |n|
+
+    if n % 3 == 0 && n % 5 == 0
+        puts "FizzBuzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    else
+        puts n
+    end
+end
