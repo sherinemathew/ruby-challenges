630376bf6cf003904192023b376597fbc220fe44
sherinemathew <sherine.mary.mathew@gmail.com>
2019-06-04 16:00:48 +1000

    challenge done

--- 15_change_letters.rb
+++ 15_change_letters.rb
@@ -15,7 +15,20 @@
 
 # Check your solution by running:
 # ruby 15_change_letters.rb
+#--------------------------------------------------------------------------------
+puts
+puts "Enter a string input"
+input = gets.strip
 
+word = input.split("")
 
-
-# Your code here
+new_word = []
+word.each do |letter|
+    if letter == "s"
+        new_word << "th"
+    else
+        new_word << letter
+    end
+end
+puts
+puts new_word.join("")
