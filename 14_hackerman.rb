ca3390b9972ca395bf3e232a2a498e7b08080cf9
sherinemathew <sherine.mary.mathew@gmail.com>
2019-06-04 13:01:43 +1000

    challenge done

--- 14_hackerman.rb
+++ 14_hackerman.rb
@@ -15,12 +15,62 @@
 # Check your solution by running:
 # ruby 14_hackerman.rb
 
-
-
-# Your code here
-
-
-
 # Beast mode:
 # Does your code deal with capital letters? Update your solution so that
 # they will be downsized if they are input but HIDDEN remains capitalised.
+
+# ---------------------------------------------------------------------------------
+puts "Enter some words"
+first = gets.strip
+
+puts "Enter some words"
+second = gets.strip
+
+def check_words(first, second)
+    first = first.downcase.split(" ")
+    second = second.downcase.split(" ")
+    output = []
+    for word in first
+        if second.include?(word)
+            output << "HIDDEN"
+        else
+            output << word
+        end
+    end
+    return output.join(" ")
+end
+
+puts check_words(first,second)
+
+# -------------------------------------------------------------------------
+
+# def collectinput
+#     puts "Please type 3 or 4 words"
+#     input = gets.strip
+#     system("clear")
+#     return input
+# end
+    
+# def protect_same(input_one, input_two)
+#     #for comparing purposes put inputs into an array
+#     input_one = input_one.downcase.split(" ")
+#     input_two = input_two.downcase.split(" ")
+
+#     #Initialize empty array for first input
+#     output = []
+
+#     #Iterate over input_one and compare to input_two
+#     for word in input_one
+#         if input_two.include?(word)
+#             output << "HIDDEN"
+#         else
+#             output << word
+#         end
+#     end
+#     return output.join(" ")
+# end
+
+# input_one = collectinput
+# input_two = collectinput
+
+# puts protect_same(input_one, input_two)
