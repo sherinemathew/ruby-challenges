f3975c66683bb4f0119d5a3f9378f96d8fac9023
sherinemathew <sherine.mary.mathew@gmail.com>
2019-06-05 19:54:48 +1000

    challenge done

--- find_errors.rb
+++ find_errors.rb
@@ -8,28 +8,29 @@
 
 
 # returns true if the arr contains the val
-def contains?(arr,val)
+def contains(arr,val)
     # Initialise the index
-    ind=0
+    ind = 0
+    found = false
     # while we haven't found the value and we 
     # still have values to check
     while !found and ind < arr.length do
         if (arr[ind] == val)
-            found=true
+            found = true
         else
             return false
         end
         # increment the index
-        ind +1
+        ind += 1
     end
     #return true or false, depending on if the value was found
     return found
 end
 
-arr=[1,2,3]
+arr = [1,2,3]
 #test when val is the first element
-puts contains?(arr,1)
+puts contains(arr,1)
 #test when val is not the first element
-puts contains?(arr,2)
+puts contains(arr,2)
 #test when val isn't in the arr
-puts contains?(arr,0)
+puts contains(arr,0)
